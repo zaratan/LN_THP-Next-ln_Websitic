a4950f9e456cc257ec91d6793bf0ea879300f0d9
HELENE-AUERBACH <ln_au@yahoo.fr>
2021-08-13 00:38:52 +0200

    Addition of saving theme in the localStorage.

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -3,10 +3,15 @@ import React, { createContext, useState } from 'react';
 export const ThemeContext = createContext();
 
 const ThemeContextProvider = (props) => {
-  const [theme, setTheme] = useState(true);
+  const themeLS = window.localStorage.getItem("theme");
+  console.log({themeLS});
+  const themeInit = (!themeLS || themeLS === "") ? true : JSON.parse(themeLS);
+  console.log({themeInit});
+  const [theme, setTheme] = useState(themeInit);
    
   const toggleTheme = () => {
     setTheme(!theme);
+    window.localStorage.setItem("theme", JSON.stringify(!theme));
   }
    
   return (
